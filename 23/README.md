c7c39a559b9b6cb940f998fdc5140048b853c673
Isadora Aguiar <isaadoraguiar@hotmail.com>
2023-10-11 23:39:21 -0300

    add project 23

---
+++ 23/README.md
@@ -0,0 +1,44 @@
+<h1 align="center"> Multi Step Form </h1>
+
+<p align="center">
+Projeto de um formulário dividido em várias etapas. Esse tipo de componente pode ser usado em formulários longos ou processos complexos para melhorar a usabilidade e experiência de usuário. <br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img src=".github/preview.jpg" width="100%">
+</p>
+
+<h2 align="center"><a href="https://isadoraguiar.github.io/bora-codar/23" target="_blank">Projeto ao vivo</a></h2>
+
+### 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- Javascript
+- Figma
+
+### 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/community/file/1248257890741817885). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+### :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Feito com ♥ por Isadora Aguiar :wave:
---
+++ 23/main.js
@@ -0,0 +1,81 @@
+let currentStep = 0
+const formSteps = document.querySelectorAll('.form-step')
+const form = document.querySelector('form')
+
+/* steps */
+form.addEventListener('click', e => {
+  if (!e.target.matches('[data-action]')) {
+    return
+  }
+
+  const actions = {
+    next() {
+      if (!isValidInputs()) {
+        return
+      }
+
+      currentStep++
+    },
+    prev() {
+      currentStep--
+    }
+  }
+
+  const action = e.target.dataset.action
+  actions[action]()
+  updateActiveStep()
+  updateProgressStep()
+})
+
+/* form submit */
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+  alert(`Obrigado ${data.get('name')}!`)
+})
+
+/* update steps */
+function updateActiveStep() {
+  formSteps.forEach(step => step.classList.remove('active'))
+  formSteps[currentStep].classList.add('active')
+}
+
+const progressStep = document.querySelectorAll('.step-progress [data-step]')
+function updateProgressStep() {
+  progressStep.forEach((step, idx) => {
+    step.classList.remove('active')
+    step.classList.remove('done')
+
+    if (idx < currentStep + 1) {
+      step.classList.add('active')
+    }
+
+    if (idx < currentStep) {
+      step.classList.add('done')
+    }
+  })
+}
+
+/* validation */
+function isValidInputs() {
+  const currentFormStep = formSteps[currentStep]
+  const formFields = [
+    ...currentFormStep.querySelectorAll('input'),
+    ...currentFormStep.querySelectorAll('textarea')
+  ]
+
+  return formFields.every(input => input.reportValidity())
+}
+
+/* animation */
+formSteps.forEach(formStep => {
+  function addHide() {
+    formStep.classList.add('hide')
+  }
+
+  formStep.addEventListener('animationend', e => {
+    addHide()
+    formSteps[currentStep].classList.remove('hide')
+  })
+})
